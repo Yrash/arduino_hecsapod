c0e3f46f813edc4901457887e5fbd2bd0f46eb24
yrash <yrash1990@gmail.com>
2019-08-12 00:50:14 +0300

    add import file

--- heksapod/heksapod.ino
+++ heksapod/heksapod.ino
@@ -1,190 +1,90 @@
-/***************************************************
-  This is an example for our Adafruit 16-channel PWM & Servo driver
-  Servo test - this will drive 8 servos, one after the other on the
-  first 8 pins of the PCA9685
-
-  Pick one up today in the adafruit shop!
-  ------> http://www.adafruit.com/products/815
-
-  These drivers use I2C to communicate, 2 pins are required to
-  interface.
-
-  Adafruit invests time and resources providing this open source code,
-  please support Adafruit and open-source hardware by purchasing
-  products from Adafruit!
-
-  Written by Limor Fried/Ladyada for Adafruit Industries.
-  BSD license, all text above must be included in any redistribution
- ****************************************************/
-
 #include <Wire.h>
-#include <Adafruit_PWMServoDriver.h>
 
-// called this way, it uses the default address 0x40
-Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
-// you can also call it with a different address you want
-//Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver(0x41);
-// you can also call it with a different address and I2C interface
-//Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver(&Wire, 0x40);
-
-// Depending on your servo make, the pulse width min and max may vary, you
-// want these to be as small/large as possible without hitting the hard stop
-// for max range. You'll have to tweak them as necessary to match the servos you
-// have!
-#define SERVOMIN 150 // this is the 'minimum' pulse length count (out of 4096)
-#define SERVOMAX 600 // this is the 'maximum' pulse length count (out of 4096)
+#include "positions.h"
 
 unsigned long periodRuning;
 
-int LEFT_HAND_FIRST = 0;
-int LEFT_HAND_TWO = 1;
-
-int RIGHT_HAND_FIRST = 2;
-int RIGHT_HAND_TWO = 3;
-
-int BOTTOM_LEFT_HAND_FIRST = 4;
-int BOTTOM_LEFT_HAND_TWO = 5;
 
 void setup()
 {
-    Serial.begin(9600);
-    Serial.println("8 cha");
-
     pwm.begin();
-
-    pwm.setPWMFreq(60); // Analog servos run at ~60 Hz updates
+    pwm.setPWMFreq(60);
 }
 
-void setServo(int port, int angle)
-{
-
-    pwm.setPWM(port, 0, angle);
+void delyServo(void runFunc(), int currentdely, int currentdely1) {
+  if (periodRuning > currentdely && periodRuning < currentdely1) {
+    runFunc();
+  }
 }
 
-// --------------------- two hand ------------
-// left Hand and two Servo
-// max 200
-//  min 150
-int maxTwoHand = 200;
-int minTwoHand = 150;
-
-void leftHandTwoUp()
-{
-
-    setServo(LEFT_HAND_TWO, minTwoHand);
-}
-void leftHandTwoDown()
-{
-    setServo(LEFT_HAND_TWO, maxTwoHand);
-}
-// bottom left  Hand and two servo
-// max 200
-//  min 150
-void bottomLeftTwoServoUp()
-{
-    setServo(BOTTOM_LEFT_HAND_TWO, minTwoHand);
-}
-void bottomLeftTwoDown()
-{
-    setServo(BOTTOM_LEFT_HAND_TWO, maxTwoHand);
-}
-
-// right Hand and two servo
-// max 200
-//  min 150
-void rightHandTwoUp()
-{
-    setServo(RIGHT_HAND_TWO, minTwoHand);
-}
-void rightHandTwoDown()
-{
-    setServo(RIGHT_HAND_TWO, maxTwoHand);
-}
-
-// --------------------- two hand ------------
-// --------------------- first hand ------------
-
-// left Hand and first servo
-// max 350
-//  min 150
-void leftHandUp()
-{
-    setServo(LEFT_HAND_FIRST, 350);
-}
-void leftHandDown()
-{
-    setServo(LEFT_HAND_FIRST, 150);
-}
-
-// right Hand and first servo
-// max 150
-//  min 350
-void rightHandUp()
-{
-    setServo(RIGHT_HAND_FIRST, 150); // 150
-}
-void rightHandDown()
-{
-    setServo(RIGHT_HAND_FIRST, 350);
-}
-
-// bottom left Hand and one servo
-// max 350
-//  min 150
-void bottomLeftOneServoUp()
-{
-    setServo(BOTTOM_LEFT_HAND_FIRST, 350); // 350
-}
-void bottomLeftOneServoDown()
-{
-    setServo(BOTTOM_LEFT_HAND_FIRST, 150);
-}
-// --------------------- first hand ------------
-
-void delyServo(void runFunc(), int currentdely, int currentdely1)
-{
-    if (periodRuning > currentdely && periodRuning < currentdely1)
-    {
-        runFunc();
-    }
-}
-
-void LeftHand()
-{
+void LeftHand() {
     //  Поднять ногу в верх
-    delyServo(leftHandTwoUp, 0, 1000);
+    delyServo(TOP_LEFT_TWO_MIN, 0, 1000);
     //  //  Опустить ногу в низ
-    delyServo(leftHandTwoDown, 1000, 2000);
+    delyServo(TOP_RIGHT_TWO_MAX, 1000, 2000);
     //  Предвинуть ногу в перед
-    delyServo(leftHandUp, 0, 2000);
+    delyServo(TOP_LEFT_FIRST_MAX, 0, 2000);
     //  Отодвинуть ногу назад
-    delyServo(leftHandDown, 2000, 4000);
+    delyServo(TOP_LEFT_FIRST_MIN, 2000, 4000);
 }
 
-void rightHand()
-{
+void rightHand() {
     //  Поднять ногу в верх
-    delyServo(rightHandTwoUp, 4000, 5000);
+    delyServo(TOP_RIGHT_TWO_MIN, 4000, 5000);
     //  Поднять ногу в низ
-    delyServo(rightHandTwoDown, 5000, 6000);
+    delyServo(TOP_RIGHT_TWO_MAX, 5000, 6000);
     //  Предвинуть ногу в перед
-    delyServo(rightHandUp, 4000, 6000);
+    delyServo(TOP_RIGHT_FIRST_MIN, 4000, 6000);
     //  Предвинуть ногу в назад
-    delyServo(rightHandDown, 6000, 8000);
+    delyServo(TOP_RIGHT_FIRST_MAX, 6000, 8000);
 }
 
-void leftButtonHand()
-{
-    delyServo(bottomLeftTwoServoUp, 8000, 9000);
-    delyServo(bottomLeftTwoDown, 9000, 10000);
+void leftButtonHand() {
+    delyServo(BOTTOM_LEFT_TWO_MIN, 8000, 9000);
+    delyServo(BOTTOM_LEFT_TWO_MAX, 9000, 10000);
 
-    delyServo(bottomLeftOneServoUp, 8000, 10000);
-    delyServo(bottomLeftOneServoDown, 10000, 12000);
+    delyServo(BOTTOM_LEFTT_FIRST_MAX, 8000, 10000);
+    delyServo(BOTTOM_LEFTT_FIRST_MIN, 10000, 12000);
+}
+
+void runToForward() {
+        //  Поднять ногу в верх
+    delyServo(TOP_LEFT_TWO_MIN, 0, 1000);
+    //  //  Опустить ногу в низ
+    delyServo(TOP_RIGHT_TWO_MAX, 1000, 2000);
+
+      //  Предвинуть ногу в перед
+    delyServo(TOP_LEFT_FIRST_MAX, 0, 2000);
+    //  Отодвинуть ногу назад
+    delyServo(TOP_LEFT_FIRST_MIN, 2000, 4000);
+
+
+
+
+    delyServo(BOTTOM_LEFT_TWO_MIN, 8000, 9000);
+    delyServo(BOTTOM_LEFT_TWO_MAX, 9000, 10000);
+
+    delyServo(BOTTOM_LEFTT_FIRST_MAX, 8000, 10000);
+    delyServo(BOTTOM_LEFTT_FIRST_MIN, 10000, 12000);
+
+        //  Поднять ногу в верх
+    delyServo(TOP_RIGHT_TWO_MIN, 4000, 5000);
+    //  Поднять ногу в низ
+    delyServo(TOP_RIGHT_TWO_MAX, 5000, 6000);
+
+          //  Предвинуть ногу в перед
+    delyServo(TOP_RIGHT_FIRST_MIN, 4000, 6000);
+    //  Предвинуть ногу в назад
+    delyServo(TOP_RIGHT_FIRST_MAX, 6000, 8000);
+      //  Предвинуть ногу в перед
+      
+
+      
 }
 
 void loop()
 {
-
+    
     if (periodRuning < 12000)
     {
         periodRuning = periodRuning + 5;
@@ -194,7 +94,7 @@ void loop()
         periodRuning = 0;
     }
     // Left hand
-    LeftHand();
-    rightHand();
-    leftButtonHand();
+    // LeftHand();
+    // rightHand();
+    // leftButtonHand();
 }
---
+++ heksapod/positions.h
@@ -0,0 +1,92 @@
+#include <Adafruit_PWMServoDriver.h>
+
+Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
+
+int TOP_LEFT_FIRST = 0;
+int TOP_LEFT_TWO = 1;
+
+int TOP_RIGHT_FIRST = 2;
+int TOP_RIGHT_TWO = 3;
+
+int BOTTOM_LEFT_FIRST = 4;
+int BOTTOM_LEFT_TWO = 5;
+
+int BOTTOM_RIGHT_FIRST = 7;
+int BOTTOM_RIGHT_TWO = 8;
+
+int MAX_TWO = 200;
+int MIN_TWO = 150;
+
+int MAX_ONE = 350;
+int MIN_ONE = 150;
+
+void setServo(int port, int angle)
+{
+  pwm.setPWM(port, 0, angle);
+}
+
+void TOP_LEFT_TWO_MIN() {
+  setServo(TOP_LEFT_TWO, MIN_TWO);
+}
+void TOP_LEFT_TWO_MAX() {
+  setServo(TOP_LEFT_TWO, MAX_TWO);
+}
+// bottom left  Hand and two servo
+// max 200
+//  min 150
+void BOTTOM_LEFT_TWO_MIN() {
+  setServo(BOTTOM_LEFT_TWO, MIN_TWO);
+}
+void BOTTOM_LEFT_TWO_MAX() {
+  setServo(BOTTOM_LEFT_TWO, MAX_TWO);
+}
+
+// right Hand and two servo
+// max 200
+//  min 150
+void TOP_RIGHT_TWO_MIN() {
+  setServo(TOP_RIGHT_TWO, MIN_TWO);
+}
+void TOP_RIGHT_TWO_MAX() {
+  setServo(TOP_RIGHT_TWO, MAX_TWO);
+}
+
+// --------------------- two hand ------------
+// --------------------- first hand ------------
+
+// left Hand and first servo
+// max 350
+//  min 150
+void TOP_LEFT_FIRST_MAX() {
+  setServo(TOP_LEFT_FIRST, MAX_ONE);
+}
+void TOP_LEFT_FIRST_MIN() {
+  setServo(TOP_LEFT_FIRST, MIN_ONE);
+}
+
+// right Hand and first servo
+// max 150
+//  min 350
+void TOP_RIGHT_FIRST_MIN() {
+  setServo(TOP_RIGHT_FIRST, MIN_ONE); // 150
+}
+void TOP_RIGHT_FIRST_MAX() {
+  setServo(TOP_RIGHT_FIRST, MAX_ONE);
+}
+
+// bottom left Hand and one servo
+// max 350
+//  min 150
+void BOTTOM_LEFTT_FIRST_MAX() {
+  setServo(BOTTOM_LEFT_FIRST, MAX_ONE); // 350
+}
+void BOTTOM_LEFTT_FIRST_MIN() {
+  setServo(BOTTOM_LEFT_FIRST, MIN_ONE);
+}
+
+void BOTTOM_RIGHT_FIRST_MAX() {
+  setServo(BOTTOM_RIGHT_FIRST, MAX_ONE); // 350
+}
+void BOTTOM_RIGHT_FIRST_MIN() {
+  setServo(BOTTOM_RIGHT_FIRST, MIN_ONE);
+}
