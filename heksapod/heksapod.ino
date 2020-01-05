a5c9d163f3b151c12cf55fc5f933f83f67d0e926
Yrash <yrash1990@gmail.com>
2020-01-05 23:01:49 +0200

    change logic

--- heksapod/heksapod.ino
+++ heksapod/heksapod.ino
@@ -1,100 +1,104 @@
 #include <Wire.h>
-
 #include "positions.h"
 
 unsigned long periodRuning;
 
+unsigned long speedGo = 8;
+unsigned long formTime = 0;
 
+String currentComand = "";
 void setup()
 {
-    pwm.begin();
-    pwm.setPWMFreq(60);
+  
+  pwm.begin();
+  pwm.setPWMFreq(50);
+  Serial.begin(9600);
+  Serial.print("Start program");
 }
 
-void delayServo(void runFunc(), int currentdely, int currentdely1) {
-  if (periodRuning > currentdely && periodRuning < currentdely1) {
+void delayServo(void runFunc(), int currentdely, int currentdely1)
+{
+  if (periodRuning > currentdely && periodRuning < currentdely1)
+  {
     runFunc();
   }
 }
 
-void LeftHand() {
-    //  Поднять ногу в верх
-    delayServo(TOP_LEFT_TWO_MIN, 0, 1000);
-    //  //  Опустить ногу в низ
-    delayServo(TOP_RIGHT_TWO_MAX, 1000, 2000);
-    //  Предвинуть ногу в перед
-    delayServo(TOP_LEFT_FIRST_MAX, 0, 2000);
-    //  Отодвинуть ногу назад
-    delayServo(TOP_LEFT_FIRST_MIN, 2000, 4000);
-}
-
-void rightHand() {
-    //  Поднять ногу в верх
-    delayServo(TOP_RIGHT_TWO_MIN, 4000, 5000);
-    //  Поднять ногу в низ
-    delayServo(TOP_RIGHT_TWO_MAX, 5000, 6000);
-    //  Предвинуть ногу в перед
-    delayServo(TOP_RIGHT_FIRST_MIN, 4000, 6000);
-    //  Предвинуть ногу в назад
-    delayServo(TOP_RIGHT_FIRST_MAX, 6000, 8000);
-}
-
-void leftButtonHand() {
-    delayServo(BOTTOM_LEFT_TWO_MIN, 8000, 9000);
-    delayServo(BOTTOM_LEFT_TWO_MAX, 9000, 10000);
-
-    delayServo(BOTTOM_LEFT_FIRST_MAX, 8000, 10000);
-    delayServo(BOTTOM_LEFT_FIRST_MIN, 10000, 12000);
-}
-
-void runToForward() {
-        //  Поднять ногу в верх
-    delayServo(TOP_LEFT_TWO_MIN, 0, 1000);
-    //  //  Опустить ногу в низ
-    delayServo(TOP_RIGHT_TWO_MAX, 1000, 2000);
-
-      //  Предвинуть ногу в перед
-    delayServo(TOP_LEFT_FIRST_MAX, 0, 2000);
-    //  Отодвинуть ногу назад
-    delayServo(TOP_LEFT_FIRST_MIN, 2000, 4000);
-
-
-
-
-    delayServo(BOTTOM_LEFT_TWO_MIN, 8000, 9000);
-    delayServo(BOTTOM_LEFT_TWO_MAX, 9000, 10000);
-
-    delayServo(BOTTOM_LEFT_FIRST_MAX, 8000, 10000);
-    delayServo(BOTTOM_LEFT_FIRST_MIN, 10000, 12000);
-
-        //  Поднять ногу в верх
-    delayServo(TOP_RIGHT_TWO_MIN, 4000, 5000);
-    //  Поднять ногу в низ
-    delayServo(TOP_RIGHT_TWO_MAX, 5000, 6000);
-
-          //  Предвинуть ногу в перед
-    delayServo(TOP_RIGHT_FIRST_MIN, 4000, 6000);
-    //  Предвинуть ногу в назад
-    delayServo(TOP_RIGHT_FIRST_MAX, 6000, 8000);
-      //  Предвинуть ногу в перед
-      
-
-      
-}
-
 void loop()
 {
-    
-    if (periodRuning < 12000)
-    {
-        periodRuning = periodRuning + 1;
-    }
-    else
-    {
-        periodRuning = 0;
-    }
-    // Left hand
-    // LeftHand();
-    // rightHand();
-    // leftButtonHand();
+
+  if (periodRuning < formTime)
+  {
+    periodRuning = periodRuning + speedGo;
+  }
+  else
+  {
+    periodRuning = 0;
+  }
+  if (Serial.available() > 0)
+  {
+    currentComand = Serial.readString();
+  }
+  Serial.println(currentComand);
+  if (currentComand.indexOf("start") > -1)
+  {
+    formTime = 2100;
+    delayServo(TOP_LEFT_TWO_MIN, 500, 600);
+    delayServo(TOP_LEFT_FIRST_MIN, 0, 500);
+    delayServo(TOP_LEFT_FIRST_MAX, 500, 1000);
+    delayServo(TOP_LEFT_TWO_MAX, 1000, 1100);
+
+    delayServo(TOP_RIGHT_TWO_MIN, 1500, 1600);
+    delayServo(TOP_RIGHT_FIRST_MIN, 1000, 1500);
+    delayServo(TOP_RIGHT_FIRST_MAX, 1500, 2000);
+    delayServo(TOP_RIGHT_TWO_MAX, 2000, 2100);
+
+    delayServo(BOTTOM_RIGHT_FIRST_MIN, 500, 1000);
+    delayServo(BOTTOM_RIGHT_FIRST_MAX, 0, 500);
+
+    delayServo(BOTTOM_LEFT_FIRST_MAX, 1000, 1500);
+    delayServo(BOTTOM_LEFT_FIRST_MIN, 1500, 2000);
+  }
+
+  if (currentComand.indexOf("read") > -1)
+  {
+    Serial.println("Start read");
+    Serial.print("TOP_LEFT_FIRST: - ");
+    Serial.println(digitalRead(TOP_LEFT_FIRST));
+  }
+  if (currentComand.indexOf("sDown") > -1) {
+    TOP_LEFT_SECOND_DOWN();
+    TOP_RIGHT_SECOND_DOWN();
+    TOP_LEFT_FIRST_MAX();
+    TOP_RIGHT_FIRST_MAX();
+    BOTTOM_LEFT_FIRST_MAX();
+    BOTTOM_RIGHT_FIRST_MAX();
+  }
+  if (currentComand.indexOf("sMin") > -1)
+  {
+    Serial.println("Start installation");
+    TOP_LEFT_FIRST_MIN();
+    TOP_LEFT_TWO_MIN();
+    TOP_RIGHT_FIRST_MIN();
+    TOP_RIGHT_TWO_MIN();
+    BOTTOM_LEFT_FIRST_MIN();
+    BOTTOM_RIGHT_FIRST_MIN();
+  }
+  if (currentComand.indexOf("sMax") > -1)
+  {
+    Serial.println("Start installation");
+    TOP_LEFT_FIRST_MAX();
+    TOP_LEFT_TWO_MAX();
+    TOP_RIGHT_FIRST_MAX();
+    TOP_RIGHT_TWO_MAX();
+    BOTTOM_LEFT_FIRST_MAX();
+    BOTTOM_RIGHT_FIRST_MAX();
+  }
+  if (currentComand.indexOf("testMax") > -1) {
+    servoRotation(0, 0, 4000);
+    delay(1000);
+    servoRotation(0, 4000, 1000);
+    delay(1000);
+    servoRotation(0, 1000, 4000);
+  }
 }
--- heksapod/positions.h
+++ heksapod/positions.h
@@ -11,8 +11,8 @@ int TOP_RIGHT_TWO = 3;
 int BOTTOM_LEFT_FIRST = 4;
 int BOTTOM_LEFT_TWO = 5;
 
-int BOTTOM_RIGHT_FIRST = 7;
-int BOTTOM_RIGHT_TWO = 8;
+int BOTTOM_RIGHT_FIRST = 6;
+int BOTTOM_RIGHT_TWO = 7;
 
 int MAX_TWO = 200;
 int MIN_TWO = 150;
@@ -24,32 +24,69 @@ void setServo(int port, int angle)
 {
   pwm.setPWM(port, 0, angle);
 }
+// if from = 150 and to = 350 
+void servoRotation (int port, int from, int to) {
+  if (from > to) {
+     for (uint16_t microsec = from; microsec > to; microsec--) {
+        pwm.writeMicroseconds(port, microsec);
+      }
+  } else {
+    for (uint16_t microsec = from; microsec < to; microsec++) {
+      pwm.writeMicroseconds(port, microsec);
+    }
+  }
+}
 
+// left hand first
+void TOP_LEFT_FIRST_MAX() {
+  setServo(TOP_LEFT_FIRST, 350);
+}
+void TOP_LEFT_FIRST_MIN() {
+  setServo(TOP_LEFT_FIRST, 150);
+}
+
+// left hand second
 void TOP_LEFT_TWO_MIN() {
-  setServo(TOP_LEFT_TWO, MIN_TWO);
+  setServo(TOP_LEFT_TWO, 400);
 }
 void TOP_LEFT_TWO_MAX() {
-  setServo(TOP_LEFT_TWO, MAX_TWO);
+  setServo(TOP_LEFT_TWO, 500);
 }
-// bottom left  Hand and two servo
-// max 200
-// min 150
-void BOTTOM_LEFT_TWO_MIN() {
-  setServo(BOTTOM_LEFT_TWO, MIN_TWO);
-}
-void BOTTOM_LEFT_TWO_MAX() {
-  setServo(BOTTOM_LEFT_TWO, MAX_TWO);
+void TOP_LEFT_SECOND_DOWN() {
+  setServo(TOP_LEFT_TWO, 300);
 }
 
-// right Hand and two servo
-// max 200
-// min 150
+// bottom left hand
+void BOTTOM_LEFT_FIRST_MAX() {
+  setServo(BOTTOM_LEFT_FIRST, MIN_TWO); // 350
+}
+void BOTTOM_LEFT_FIRST_MIN() {
+  setServo(BOTTOM_LEFT_FIRST, 350);
+}
+
+// right hand
+void TOP_RIGHT_FIRST_MIN() {
+  setServo(TOP_RIGHT_FIRST, 350); // 150
+}
+void TOP_RIGHT_FIRST_MAX() {
+  setServo(TOP_RIGHT_FIRST, 150);
+}
+// Rignt hand second
 void TOP_RIGHT_TWO_MIN() {
-  setServo(TOP_RIGHT_TWO, MIN_TWO);
+  setServo(TOP_RIGHT_TWO, 400);
 }
 void TOP_RIGHT_TWO_MAX() {
-  setServo(TOP_RIGHT_TWO, MAX_TWO);
+  setServo(TOP_RIGHT_TWO, 500);
 }
+void TOP_RIGHT_SECOND_DOWN() {
+  setServo(TOP_RIGHT_TWO, 300);
+}
+
+
+
+// right Hand and two servo
+
+
 
 // --------------------- two hand ------------
 // --------------------- first hand ------------
@@ -57,32 +94,17 @@ void TOP_RIGHT_TWO_MAX() {
 // left Hand and first servo
 // max 350
 // min 150
-void TOP_LEFT_FIRST_MAX() {
-  setServo(TOP_LEFT_FIRST, MAX_ONE);
-}
-void TOP_LEFT_FIRST_MIN() {
-  setServo(TOP_LEFT_FIRST, MIN_ONE);
-}
+
 
 // right Hand and first servo
 // max 150
 //  min 350
-void TOP_RIGHT_FIRST_MIN() {
-  setServo(TOP_RIGHT_FIRST, MIN_ONE); // 150
-}
-void TOP_RIGHT_FIRST_MAX() {
-  setServo(TOP_RIGHT_FIRST, MAX_ONE);
-}
+
 
 // bottom left Hand and one servo
 // max 350
 // min 150
-void BOTTOM_LEFT_FIRST_MAX() {
-  setServo(BOTTOM_LEFT_FIRST, MAX_ONE); // 350
-}
-void BOTTOM_LEFT_FIRST_MIN() {
-  setServo(BOTTOM_LEFT_FIRST, MIN_ONE);
-}
+
 
 void BOTTOM_RIGHT_FIRST_MAX() {
   setServo(BOTTOM_RIGHT_FIRST, MAX_ONE); // 350
